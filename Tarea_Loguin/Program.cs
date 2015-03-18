655924fba22ed2233f8ad8cad05e87c2a046d0f5
minermor <antony>
2015-03-18 04:13:52 -0600

    Tarea de Programacion IV

---
+++ Tarea_Loguin/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Miny
+ * Date: 17/03/2015
+ * Time: 09:25 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Tarea_Loguin
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
